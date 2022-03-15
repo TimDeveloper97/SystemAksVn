3ae370032216ecf46177072d9a2844b0a9bb1f23
Tim1997 <timbkhn@gmail.com>
2022-03-15 11:54:59 +0700

    update view history pag

--- System_aks_vn/System_aks_vn/Domain/BaseApi.cs
+++ System_aks_vn/System_aks_vn/Domain/BaseApi.cs
@@ -10,8 +10,9 @@ namespace System_aks_vn.Domain
         public const string DeviceList = "service/getDeviceList";
         public const string DeviceStatus = "remote/getDeviceStatus";
         public const string Control = "remote/control";
-        public const string SettingSms = "setting/sms";
-        public const string SettingCall = "setting/call";
+        public const string SettingSms = "setting/phone";
+        public const string SettingCall = "setting/phone";
         public const string SettingPlan = "setting/plan";
+        public const string DeviceHistory = "remote/getHistory";
     }
 }
--- System_aks_vn/System_aks_vn/ViewModels/Devices/DeviceHistoryViewModel.cs
+++ System_aks_vn/System_aks_vn/ViewModels/Devices/DeviceHistoryViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System_aks_vn.Controls;
 using System_aks_vn.Domain;
+using System_aks_vn.Models;
+using System_aks_vn.Models.View;
 using Xamarin.Forms;
 
 namespace System_aks_vn.ViewModels.Devices
@@ -15,6 +21,7 @@ namespace System_aks_vn.ViewModels.Devices
     {
         #region Property
         private string parameterDeviceId;
+        private ObservableCollection<DeviceHistoryModel> historys;
 
         public string ParameterDeviceId
         {
@@ -22,39 +29,97 @@ namespace System_aks_vn.ViewModels.Devices
             set
             {
                 parameterDeviceId = Uri.UnescapeDataString(value ?? string.Empty);
-                Title = $"History [{parameterDeviceId}]";
+                Title = $"{parameterDeviceId}";
                 SetProperty(ref parameterDeviceId, value);
             }
         }
+        public ObservableCollection<DeviceHistoryModel> Historys { get => historys; set => SetProperty(ref historys, value); }
+
         #endregion
 
         #region Command 
         public ICommand PageAppearingCommand => new Command(async () =>
         {
-            Init();
-
-            await ExecuteLoadDeviceStatusCommand();
+            DependencyService.Get<IStatusBar>().SetColoredStatusBar("#007bff");
+            IsBusy = true;
         });
+
+        public ICommand LoadHistoryDeviceCommand { get; set; }
         #endregion
 
         public DeviceHistoryViewModel()
         {
+            Init();
         }
 
         #region Method
         void Init()
         {
-            DependencyService.Get<IStatusBar>().SetColoredStatusBar("#007bff");
-            IsBusy = true;
+            Historys = new ObservableCollection<DeviceHistoryModel>();
+            LoadHistoryDeviceCommand = new Command(async () => await ExecuteLoadHistoryCommand());
         }
 
-        async Task ExecuteLoadDeviceStatusCommand()
+        async Task ExecuteLoadHistoryCommand()
         {
+            var loadingDialog = await XF.Material.Forms.UI.Dialogs.MaterialDialog.Instance
+                    .LoadingDialogAsync(message: $"Loading");
             IsBusy = true;
 
             try
             {
+                Historys?.Clear();
+                Mqtt.ClearEvent();
+                Mqtt.Publish(Topic, new DeviceContext
+                {
+                    Url = Api.DeviceHistory,
+                    Token = Token,
+                    DeviceId = ParameterDeviceId,
+                });
 
+                Mqtt.MessageReceived += async (s, e) =>
+                {
+                    var res = (s as Mqtt).Response;
+                    if (res.Code == 100)
+                        await TimeoutSession(res.Message);
+
+                    var month = JObject.Parse(res.Value.ToString())[DateTime.Now.ToString("yyyyMM")].ToString();
+                    if (month == null) return;
+
+                    var lhistory = JsonConvert.DeserializeObject<List<HistoryResponse>>(month);
+                    var ldetail = new List<Details>();
+
+                    // get list command
+                    foreach (var item in lhistory)
+                    {
+                        var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(item.Content.ToString());
+                        var content = "";
+
+                        foreach (var kv in dict)
+                        {
+                            content += kv.Key + ", ";
+                        }
+                        content = content.Substring(0, content.Length - 2);
+
+                        // add to list temp
+                        ldetail.Add(new Details { Name = content, Time = item.Time });
+                    }
+
+                    var groups = ldetail.GroupBy(x => x.Time.Date).Select(y => new DeviceHistoryModel
+                    {
+                        Name = y.Key.ToString("yyyy-MM-dd"),
+                        Count = y.Count(),
+                        Time = y.Key,
+                        Details = new ObservableCollection<Details>(y.ToList()),
+                    });
+
+                    await Device.InvokeOnMainThreadAsync(() =>
+                    {
+                        foreach (var item in groups)
+                        {
+                            Historys.Add(item);
+                        }
+                    });
+                };
             }
             catch (Exception ex)
             {
@@ -62,9 +127,9 @@ namespace System_aks_vn.ViewModels.Devices
             }
             finally
             {
+                await loadingDialog.DismissAsync();
                 IsBusy = false;
             }
-
         }
         #endregion
     }
